e95fc36a4891e1168787bcccca6edc124c6b271e
Sean-LH <89804140+Sean-LH@users.noreply.github.com>
2024-04-17 10:39:27 -0700

    adjusted vite.config file

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -1,14 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const isDevEnv = false
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server:{
     proxy:{
       '/api':{
-        target: 'http://localhost:9000',
+        target: !isDevEnv? "https://l6-beagenie.onrender.com/" : 'http://localhost:9000',
         changeOrigin: true,
+        secure: false
       }
     }
   }
